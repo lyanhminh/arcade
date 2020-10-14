b4b35e00757d5a3ad1e1d8ff4cebca4ed8aa4327
minh ly <mly@catalyte.io>
2020-10-13 23:21:13 -0400

    Registration and login set up

--- arcade/gameroom/models.py
+++ arcade/gameroom/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import date
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class GameSession(models.Model):
+    id = models.ForeignKey(User, on_delete=models.CASCADE, primary_key=True)
+    game = models.CharField(max_length=64)
+    score = models.IntegerField()
+    date = models.DateField(default=date.today)
+
+    def __str__(self):
+        return str(self.id)
--- arcade/gameroom/urls.py
+++ arcade/gameroom/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 app_name = 'gameroom'
 
 
 urlpatterns = [
-    path('<str:loggedInUser>', views.index, name='home'),
+    path('', views.index, name='home'),
     path('play/<str:game>', views.play, name='play'),
     path('register', views.register, name='register'),
-    path('login', views.login, name='login')] 
+    path('accounts/', include('django.contrib.auth.urls'))]
